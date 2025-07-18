92885570f7b68946a4e7ca129527165bcb30e0a1
Chemini Gamage <152427327+Chemini-Gamage@users.noreply.github.com>
2025-07-18 20:40:07 +0530

    fix:comment added

--- load_iam.py
+++ load_iam.py
@@ -2,6 +2,7 @@ from datasets import load_dataset
 import matplotlib.pyplot as plt
 
 dataset = load_dataset("Teklia/IAM-line", split="train", streaming=True)
+#  the first item from the stream is printed by next(iter(dataset) 
 sample = next(iter(dataset))
 
 # Display image and label
